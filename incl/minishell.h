ba59296e9504b5ab3f21c5eced600b236f45c496
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-08-27 18:30:47 +0200

    Added structs and enums to header file

--- incl/minishell.h
+++ incl/minishell.h
@@ -13,6 +13,7 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+/* LIBRARIES */
 # include <errno.h>
 # include <fcntl.h>
 # include <readline/readline.h>
@@ -24,7 +25,99 @@
 # include <sys/wait.h>
 # include <unistd.h>
 
+/* ENUMS */
+typedef enum e_node_type
+{
+	NODE_CMD,
+	NODE_PIPE,
+	NODE_AND_IF,
+	NODE_OR_IF,
+	NODE_REDIR_IN,
+	NODE_REDIR_OUT,
+	NODE_REDIR_APPEND,
+	NODE_HEREDOC,
+	NODE_SUBSHELL
+}	t_node_type;
+
+typedef enum e_shell_state
+{
+	INTERACTIVE,
+	EXECUTING,
+	HEREDOC_MODE
+}	t_shell_state;
+
+typedef enum e_token_type
+{
+	DEFAULT,
+	WORD,
+	ASSIGNMENT,
+	QUOTE,
+	SINGLE_QUOTE,
+	DOUBLE_QUOTE,
+	REDIR,
+	REDIR_IN,
+	REDIR_OUT,
+	REDIR_APPEND,
+	HEREDOC,
+	OPERATOR,
+	PIPE,
+	AND,
+	OR,
+	SEMICOLON,
+	PARENTHESIS,
+	PAREN_OPEN,
+	PAREN_CLOSE,
+	SPECIAL_CHARACTER,
+	ENV_VAR,
+	EXIT_STATUS,
+	ESCAPE,
+	COMMENT,
+	T_NEWLINE,
+	T_EOF,
+	UNKNOWN
+}	t_token_type;
+
+/* STRUCTS */
+typedef struct s_token
+{
+	struct s_token	*next;
+	struct s_token	*previous;
+	t_token_type	type;
+	char			*content;
+	int				needs_expansion;
+}	t_token;
+
+typedef struct s_cmd
+{
+	char	**args;
+	char	*path;
+	int		fd_in;
+	int		fd_out;
+}	t_cmd;
+
+typedef struct s_shell
+{
+	char	**envp;
+	char	**paths;
+	int		exit_status;
+	int		state;
+}	t_shell;
+
+typedef struct s_ast
+{
+	struct s_ast	**children;
+	struct s_ast	*root;
+	t_node_type		type;
+	t_shell			data;
+	t_cmd			cmd;
+	char			*file;
+	int				exit_status;
+}	t_ast;
+
+/* GLOBAL VARIABLE */
 extern volatile sig_atomic_t g_signal_received;
+
+/* FUNCTIONS */
 extern void	rl_replace_line(const char *str, int i);
 
 #endif
