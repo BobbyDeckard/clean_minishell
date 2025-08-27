ece337aa8858278f81202fdf8203cda408b29aa4
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-08-27 18:20:28 +0200

    Added header file

---
+++ incl/minishell.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 17:43:29 by imeulema          #+#    #+#             */
+/*   Updated: 2025/08/27 18:20:02 by imeulema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+extern volatile sig_atomic_t g_signal_received;
+extern void	rl_replace_line(const char *str, int i);
+
+#endif
