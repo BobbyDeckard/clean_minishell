732d5f2b3d73501d0e559d804d085c339f226b9d
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-08-28 11:13:24 +0200

    Moved files to data dir

---
+++ srcs/parsing/parse_command.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_command.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 10:54:10 by imeulema          #+#    #+#             */
+/*   Updated: 2025/08/28 11:00:32 by imeulema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incl/minishell.h"
+#include <unistd.h>
+
+t_ast	*parse_command(char *command, t_shell *data)
+{
+	t_token	**token_list;
+	t_ast	*ast;
+
+	if (!command)
+		return (NULL);
+	token_list = tokenize_command(data, command);
+	if (!token_list)
+		return (NULL);	// check if tokenization errors should exit completely or not
+	if (!valid_syntax(token_list))
+	{
+		free_token_list(token_list);
+		ft_putstr_fd("Syntax error in command\n", STDERR_FILENO);
+		return (NULL);
+	}
+	ast = create_ast(token_list, data);
+	if (!ast)
+	{
+		free_token_list(token_list);
+		ft_putstr_fd("Failed to create AST\n", STDERR_FILENO);
+		return (NULL);
+	}
+	free_token_list(token_list);
+	return (ast);
+}
---
+++ srcs/parsing/tokens/tokens.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokens.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 11:03:10 by imeulema          #+#    #+#             */
+/*   Updated: 2025/08/28 11:11:26 by imeulema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../incl/minishell.h"
+
+static t_token_type	get_token_type(char c)
+{
+	if (is_command_char(c))
+		return (WORD);
+	else if (is_quote(c))
+		return (QUOTE);
+	else if (is_redirection(c))
+		return (REDIR);
+	else if (is_operator(c))
+		return (OPERATOR);
+	else if (is_parenthesis(c))
+		return (PARENTHESIS);
+	else if (is_special_character(c))
+		return (SPECIAL_CHARACTER);
+	else if (is_env_var(c))
+		return (ENV_VAR);
+	return (DEFAULT);
+}
+
+t_token	**tokenize_command(t_shell *data, char *command)
+{
+	t_token_type	type;
+	t_token			**token_list;
+	t_token			**result;
+
+	token_list = (t_token **) malloc(sizeof(t_token *));
+	if (!token_list)
+		return (NULL);
+	*token_list = NULL;
+	while (*command)
+	{
+		type = get_token_type(*command);
+		printf("tokenize_command: type for %s is %d\n", command, type);
+		if (type == DEFAULT)
+		{
+			command++;
+			continue ;
+		}
+		result = extract_token(&command, type, token_list);
+		if (!result)
+			return (free(token_list), NULL);	// wtf kind of error handling is that
+		token_list = result;
+	}
+	mark_tokens_for_expansion(token_list, data->envp);
+	return (token_list);
+}
