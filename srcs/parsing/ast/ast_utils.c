e7ad8a1c6565b48855214fbb18d40bac4339d63c
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-09-02 10:51:22 +0200

    Renamed files

--- srcs/parsing/ast/ast_utils.c
+++ srcs/parsing/ast/ast_utils.c
@@ -1,74 +1,77 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ast_utils.c                                        :+:      :+:    :+:   */
+/*   ast_utils2.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/31 15:21:20 by imeulema          #+#    #+#             */
-/*   Updated: 2025/09/01 14:09:37 by imeulema         ###   ########.fr       */
+/*   Created: 2025/09/01 11:00:51 by imeulema          #+#    #+#             */
+/*   Updated: 2025/09/01 11:39:58 by imeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../incl/minishell.h"
+#include <unistd.h>
 
-static int	get_operator_precedence(t_token_type type)
+t_ast	*create_ast_node(t_node_type type, t_shell data)
 {
-	if (type == AND || type == OR)
+	t_ast	*node;
+
+	node = (t_ast *) malloc(sizeof(t_ast));
+	if (!node)
+		return (NULL);
+	node->type = type;
+	node->file = NULL;
+	node->children = NULL;
+	node->root = NULL;
+	node->data = data;
+	node->cmd.args = NULL;
+	node->cmd.path = NULL;
+	node->cmd.fd_in = STDIN_FILENO;
+	node->cmd.fd_out = STDOUT_FILENO;
+	return (node);
+}
+
+int	is_redir_token(t_token *token)
+{
+	if (!token)
+		return (0);
+	else if (token->type == REDIR_IN)
 		return (1);
-	else if (type == PIPE)
-		return (2);
-	return (3);
+	else if (token->type == REDIR_OUT)
+		return (1);
+	else if (token->type == REDIR_APPEND)
+		return (1);
+	else if (token->type == HEREDOC)
+		return (1);
+	return (0);
 }
 
-static void	check_lowest_precedence(t_token *current, int *res, int *low_prec, int i)
+t_node_type	convert_types(t_token_type type)
 {
-	int	cur_prec;
-
-	cur_prec = get_operator_precedence(current->type);
-	if (cur_prec <= *low_prec)
-	{
-		*low_prec = cur_prec;
-		*res = i;
-	}
+	if (type == PIPE)
+		return (NODE_PIPE);
+	else if (type == AND)
+		return (NODE_AND_IF);
+	else if (type == OR)
+		return (NODE_OR_IF);
+	return (NODE_CMD);
 }
 
-int	find_lowest_precedence_op(t_token **tokens, int i, int end)
-{
-	t_token	*current;
-	int		paren_lvl;
-	int		low_prec;
-	int		cur_prec;
-	int		res;
-
-	res = -1;
-	low_prec = 2147483647;
-	paren_lvl = 0;
-	while (i <= end)
-	{
-		current = get_token_at_index(tokens, i);
-		if (!current)
-			break ;
-		else if (current->type == PAREN_OPEN)
-			paren_lvl++;
-		else if (current->type == PAREN_CLOSE)
-			paren_lvl--;
-		if (!paren_lvl && (current->type == PIPE || current->type == AND || current->type == OR))
-			check_lowest_precedence(current, &res, &low_prec, i);
-		i++;
-	}
-	return (res);
-}
-
-static int	check_between_parentheses(t_token **tokens, int start, int end)
+int	find_matching_parentheses(t_token **tokens, int open_pos, int end)
 {
 	t_token	*current;
 	int		lvl;
+	int		i;
 
-	lvl = 0;
-	while (start <= end)
+	current = get_token_at_index(tokens, open_pos);
+	if (!current || current->type != PAREN_OPEN)
+		return (-1);
+	lvl = 1;
+	i = open_pos;
+	while (++i <= end)
 	{
-		current = get_token_at_index(tokens, start);
+		current = get_token_at_index(tokens, i);
 		if (!current)
 			break ;
 		else if (current->type == PAREN_OPEN)
@@ -76,23 +79,23 @@ static int	check_between_parentheses(t_token **tokens, int start, int end)
 		else if (current->type == PAREN_CLOSE)
 		{
 			lvl--;
-			if (lvl == 0 && start != end)
-				return (0);
+			if (!lvl)
+			return (i);
 		}
-		start++;
 	}
-	return (lvl == 0);
+	return (-1);
 }
 
-int	matching_parentheses(t_token **tokens, int start, int end)
+t_token	*get_token_at_index(t_token **tokens, int index)
 {
 	t_token	*current;
+	int		i;
 
-	current = get_token_at_index(tokens, start);
-	if (!current || current->type != PAREN_OPEN)
-		return (0);
-	current = get_token_at_index(tokens, end);
-	if (!current || current->type != PAREN_CLOSE)
-		return (0);
-	return (check_between_parentheses(tokens, start, end));
+	if (!tokens || !*tokens || index < 0)
+		return (NULL);
+	current = *tokens;
+	i = -1;
+	while (current && ++i < index)
+		current = current->next;
+	return (current);
 }
