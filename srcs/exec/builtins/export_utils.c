357f37484205a85eed7856821782011947ad956a
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-10-03 16:29:09 +0200

    Solved name errors for export

--- srcs/exec/builtins/export_utils.c
+++ srcs/exec/builtins/export_utils.c
@@ -6,11 +6,34 @@
 /*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 13:16:44 by imeulema          #+#    #+#             */
-/*   Updated: 2025/09/30 18:28:56 by imeulema         ###   ########.fr       */
+/*   Updated: 2025/10/03 16:28:49 by imeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../incl/minishell.h"
+
+static int	valid_name(char *arg)
+{
+	int	alpha;
+	int	i;
+
+	i = 0;
+	alpha = 0;
+	while (arg[i] && arg[i] != '=')
+	{
+		if (!ft_isalpha(arg[i]) && arg[i] != '_' && !alpha)
+		{
+			ft_putstr_fd("minishell: '", 2);
+			ft_putstr_fd(arg, 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
+			return (0);
+		}
+		else if (ft_isalpha(arg[i]) && !alpha)
+			alpha = 1;
+		i++;
+	}
+	return (1);
+}
 
 int	handle_export_args(t_ast *node, int size)
 {
@@ -18,9 +41,14 @@ int	handle_export_args(t_ast *node, int size)
 	int	i;
 
 	status = 0;
-	i = -1;
+	i = 0;
 	while (node->cmd.args[++i])
 	{
+		if (!valid_name(node->cmd.args[i]))
+		{
+			status = 1;
+			continue ;
+		}
 		if (!ft_strncmp(node->cmd.args[i], "PATH=", 5))
 			update_paths(node, node->data, node->cmd.args[i] + 5);
 		if (has_equal(node->cmd.args[i]) && assign_var(node, size, i))
