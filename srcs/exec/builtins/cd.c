668dc5e38d14ba15f86370b3dab3f4be2ca71e1b
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-10-09 13:09:13 +0200

    Implemented basic cd -

--- srcs/exec/builtins/cd.c
+++ srcs/exec/builtins/cd.c
@@ -6,25 +6,25 @@
 /*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 16:16:45 by imeulema          #+#    #+#             */
-/*   Updated: 2025/10/08 15:55:01 by imeulema         ###   ########.fr       */
+/*   Updated: 2025/10/09 13:08:34 by imeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../incl/minishell.h"
 
-static char	*cd_error(t_ast *node)
+static char	*cd_error(t_ast *node, int arg)
 {
 	t_cmd	cmd;
 	char	*msg;
 	int		len;
 
 	cmd = node->cmd;
-	len = ft_strlen(cmd.args[1]) + 5;
+	len = ft_strlen(cmd.args[arg]) + 5;
 	msg = (char *) malloc(len * sizeof(char));
 	if (!msg)
 		malloc_error(node, node->data, NULL);
 	ft_strlcpy(msg, "cd: ", len);
-	ft_strlcat(msg, cmd.args[1], len);
+	ft_strlcat(msg, cmd.args[arg], len);
 	return (msg);
 }
 
@@ -83,10 +83,52 @@ static void	make_updates(t_ast *node, char *oldpwd)
 		return (update_oldpwd(node, j, oldpwd));
 }
 
+static int	chdir_error(t_ast *node, char *oldpwd, int arg, int in_pipe)
+{
+	char	*error;
+
+	error = cd_error(node, arg);
+	perror(error);
+	free(error);
+	free(oldpwd);
+	if (!in_pipe)
+		close_all_redirs(node);
+	return (set_exit_status(node, 1));
+}
+
+//	wtf...?
+//	Test done in this directory
+static int	print_oldpwd(t_ast *node, char **envp, int in_pipe)
+{
+	char	name[7];
+	int		i;
+	int		j;
+
+	ft_strlcpy(name, "OLDPWD", 7);
+	i = -1;
+	while (envp[++i])
+	{
+		j = 0;
+		while (name[j] && name[j] == envp[i][j])
+			j++;
+		if (envp[i][j] == '=' && !name[j])
+		{
+			ft_putstr_fd(envp[i] + 6, node->cmd.fd_out);
+			ft_putchar_fd('\n', node->cmd.fd_out);
+			if (!in_pipe)
+				close_all_redirs(node);
+			return (set_exit_status(node, 0));
+		}
+	}
+	ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2);
+	if (!in_pipe)
+		close_all_redirs(node);
+	return (set_exit_status(node, 1));
+}
+
 int	cd(t_ast *node, int in_pipe)
 {
 	char	*oldpwd;
-	char	*error;
 
 	if (!in_pipe && make_redirs(node))
 		return (set_exit_status(node, 1));
@@ -99,16 +141,10 @@ int	cd(t_ast *node, int in_pipe)
 			close_all_redirs(node);
 		return (set_exit_status(node, 0));
 	}
-	if (chdir(node->cmd.args[1]) < 0)
-	{
-		error = cd_error(node);
-		perror(error);
-		free(error);
-		free(oldpwd);
-		if (!in_pipe)
-			close_all_redirs(node);
-		return (set_exit_status(node, 1));
-	}
+	if (!ft_strncmp(node->cmd.args[1], "-", 2))
+		return (!print_oldpwd(node, node->data->envp, in_pipe));
+	else if (chdir(node->cmd.args[1]) < 0)
+		return (chdir_error(node, oldpwd, 1, in_pipe));
 	make_updates(node, oldpwd);
 	if (!in_pipe)
 		close_all_redirs(node);
