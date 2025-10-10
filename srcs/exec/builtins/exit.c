7c2963f55dc77d1d1f9a47e1c3f3288a6c6df4f5
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-10-10 16:14:56 +0200

    Solved small issue with exit args (exit "" not handled)

--- srcs/exec/builtins/exit.c
+++ srcs/exec/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 18:19:07 by imeulema          #+#    #+#             */
-/*   Updated: 2025/10/08 15:56:41 by imeulema         ###   ########.fr       */
+/*   Updated: 2025/10/10 16:13:58 by imeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static void	check_digits(t_ast *node, char *arg)
 			ft_putstr_fd(arg, 2);
 			ft_putstr_fd(": numeric argument required\n", 2);
 			cleanup(node);
-			exit(255);
+			exit(2);
 		}
 	}
 }
@@ -40,16 +40,17 @@ static void	check_length(t_ast *node, char *arg)
 		ft_putstr_fd(arg, 2);
 		ft_putstr_fd(": numeric argument required\n", 2);
 		cleanup(node);
-		exit(255);
+		exit(2);
 	}
 }
 
-static void	too_many_args_error(t_ast *node, int in_pipe)
+static int	too_many_args(t_ast *node, int in_pipe)
 {
 	ft_putstr_fd("minishell: exit: too many arguments\n", 2);
 	ft_putstr_fd(node->cmd.args[2], 2);
 	if (!in_pipe)
 		close_all_redirs(node);
+	return (set_exit_status(node, 1));
 }
 
 int	exit_bltn(t_ast *node, int in_pipe)
@@ -59,21 +60,20 @@ int	exit_bltn(t_ast *node, int in_pipe)
 	ft_putstr_fd("exit\n", node->cmd.fd_out);
 	if (!in_pipe && make_redirs(node))
 		return (set_exit_status(node, 1));
-	if (node->cmd.args[1] && node->cmd.args[2])
-		too_many_args_error(node, in_pipe);
-	else if (node->cmd.args[1])
+	if (node->cmd.args[1])
 	{
 		check_digits(node, node->cmd.args[1]);
 		check_length(node, node->cmd.args[1]);
+		if (node->cmd.args[2])
+			return (too_many_args(node, in_pipe));
 		n = ft_atol(node->cmd.args[1]) % 256;
 		cleanup(node);
-		exit(n);
+		exit (n);
 	}
 	else
 	{
-		n = node->data->exit_status;
+		n = node->data->exit_status % 256;
 		cleanup(node);
 		exit(n);
 	}
-	return (1);
 }
