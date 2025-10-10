7fe3005130916c66186239b627c9cab9d9a58757
Ivo Meulemans <imeulema@c2r9s1.42lausanne.ch>
2025-10-10 13:03:40 +0200

    Modified test script

--- valgrind_script.sh
+++ valgrind_script.sh
@@ -1,1 +1,10 @@
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --track-fds=yes --log-file=val_out.log --suppressions=ignore_readline_leaks.supp ./minishell
+echo "Currently available options are:"
+echo "	1. Builtins"
+echo -n "Please select what you want to test: "
+read option
+if [ $option = "1" ]; then
+	filename="test_builtins.txt"
+fi
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --track-fds=yes --log-file=val_out.log --suppressions=ignore_readline_leaks.supp ./minishell < $filename > minishell_output.txt
+bash < $filename > bash_output.txt
+diff minishell_output.txt bash_output.txt > output.diff
