c988a18f5b0445595c8230dea314a7fed2469867
Ivo Meulemans <imeulema@c2r9s6.42lausanne.ch>
2025-10-28 16:05:57 +0100

    Renamed files

--- srcs/exec/redirs/redir_expander.c
+++ srcs/exec/redirs/redir_expander.c
@@ -1,131 +1,91 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   redir_expander.c                                   :+:      :+:    :+:   */
+/*   redir_expander_quotes.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/20 17:07:07 by imeulema          #+#    #+#             */
-/*   Updated: 2025/10/27 20:08:13 by imeulema         ###   ########.fr       */
+/*   Created: 2025/10/27 19:59:48 by imeulema          #+#    #+#             */
+/*   Updated: 2025/10/27 20:02:02 by imeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../incl/minishell.h"
 
-void	rdr_double_expand(t_ast *node, t_rdr *rdr, char **envp, int index);
-int		contains_dol(const char *str);
-int		rdr_remove_arg(t_rdr *rdr, int i);
+int	contains_dol(const char *str);
+int	rdr_expand_cat(t_ast *node, t_rdr *rdr, char **envp, int index);
+int	rdr_handle_double_quotes(t_ast *node, t_rdr *rdr, int start);
+int	rdr_handle_single_quotes(t_ast *node, t_rdr *rdr, int start);
+int	rdr_remove_arg(t_rdr *rdr, int i);
 
-static void	single_quotes_loop(t_rdr *rdr, char *new, int st_end[2], int len)
+static int	rdr_expand(t_ast *node, t_rdr *rdr, char **envp, int index)
 {
-	int	start;
-	int	end;
-	int	i;
-
-	start = st_end[0];
-	end = st_end[1];
-	i = start;
-	while (++i < end)
+	while (contains_dol(rdr->args[index]))
 	{
-		ft_strlcat(new, rdr->args[start], len);
-		if (rdr->args[start + 1] && ft_strncmp(rdr->args[start + 1], "'", 2))
-			rdr_remove_arg(rdr, start);
-		else if (*new)
-		{
-			free(rdr->args[start]);
-			rdr->args[start] = new;
-			rdr_remove_arg(rdr, start + 1);
-			break ;
-		}
-		else
-			free(new);
+		if (rdr_expand_cat(node, rdr, envp, index))
+			return (1);
 	}
-}
-
-int	rdr_handle_single_quotes(t_ast *node, t_rdr *rdr, int start)
-{
-	char	*new;
-	int		st_end[2];
-	int		len;
-	int		end;
-
-	len = 0;
-	end = start;
-	while (rdr->args[++end] && ft_strncmp(rdr->args[end], "'", 2))
-		len += ft_strlen(rdr->args[end]);
-	rdr_remove_arg(rdr, start);
-	if (!len)
-	{
-		rdr_remove_arg(rdr, start);
-		return (1);
-	}
-	new = (char *) ft_calloc(++len, sizeof(char));
-	if (!new)
-		malloc_error(node, node->shell, NULL);
-	st_end[0] = start;
-	st_end[1] = end;
-	single_quotes_loop(rdr, new, st_end, len);
 	return (0);
 }
 
-static void	expand_inside_double(t_ast *node, t_rdr *rdr, int i)
+static int	check_arg(t_ast *node, t_rdr *rdr, int index)
 {
-	while (rdr->args[++i] && ft_strncmp(rdr->args[i], "\"", 2))
+	if (!ft_strncmp(rdr->args[index], "'", 2))
 	{
-		while (contains_dol(rdr->args[i]))
-			rdr_double_expand(node, rdr, node->shell->envp, i);
-	}
-}
-
-static void	double_quotes_loop(t_rdr *rdr, char *new, int st_end[2], int len)
-{
-	int	start;
-	int	end;
-	int	i;
-
-	start = st_end[0];
-	end = st_end[1];
-	i = start;
-	while (++i < end)
-	{
-		ft_strlcat(new, rdr->args[start], len);
-		if (rdr->args[start + 1] && ft_strncmp(rdr->args[start + 1], "\"", 2))
-			rdr_remove_arg(rdr, start);
-		else if (*new)
-		{
-			free(rdr->args[start]);
-			rdr->args[start] = new;
-			rdr_remove_arg(rdr, start + 1);
-			break ;
-		}
-		else
-			free(new);
-	}
-}
-
-int	rdr_handle_double_quotes(t_ast *node, t_rdr *rdr, int start)
-{
-	char	*new;
-	int		st_end[2];
-	int		len;
-	int		end;
-
-	len = 0;
-	end = start;
-	expand_inside_double(node, rdr, start);
-	while (rdr->args[++end] && ft_strncmp(rdr->args[end], "\"", 2))
-		len += ft_strlen(rdr->args[end]);
-	rdr_remove_arg(rdr, start);
-	if (!len)
-	{
-		rdr_remove_arg(rdr, start);
+		rdr_handle_single_quotes(node, rdr, index);
+		if (!rdr->args[index])
+			return (0);
 		return (1);
 	}
-	new = (char *) ft_calloc(++len, sizeof(char));
-	if (!new)
-		malloc_error(node, node->shell, NULL);
-	st_end[0] = start;
-	st_end[1] = end;
-	double_quotes_loop(rdr, new, st_end, len);
+	else if (!ft_strncmp(rdr->args[index], "\"", 2))
+	{
+		rdr_handle_double_quotes(node, rdr, index);
+		if (!rdr->args[index])
+			return (0);
+		return (1);
+	}
+	else if (contains_dol(rdr->args[index]))
+		return (rdr_expand(node, rdr, node->shell->envp, index));
 	return (0);
+}
+
+static char	*cat_arg(t_ast *node, char *str, char *arg)
+{
+	char	*new;
+	int		len;
+
+	len = ft_strlen(str) + ft_strlen(arg) + 1;
+	new = (char *) ft_calloc(len, sizeof(char));
+	if (!new)
+	{
+		if (str)
+			free(str);
+		malloc_error(node, node->shell, NULL);
+	}
+	if (str)
+		ft_strlcpy(new, str, len);
+	ft_strlcat(new, arg, len);
+	if (str)
+		free(str);
+	return (new);
+}
+
+void	redir_expander(t_ast *node, t_rdr *rdr)
+{
+	char	*new;
+	int		i;
+
+	i = 0;
+	new = NULL;
+	while (rdr->args[i])
+	{
+		if (check_arg(node, rdr, i))
+			continue ;
+		new = cat_arg(node, new, rdr->args[i]);
+		rdr_remove_arg(rdr, i);
+		if (!rdr->args[i + 1])
+			break ;
+	}
+	free(rdr->args[i]);
+	rdr->args[i] = new;
 }
