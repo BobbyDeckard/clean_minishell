0d15a3456a5b6e725dcbcb88beb5f4402f221090
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-10-28 16:18:42 +0100

    Added header

--- srcs/exec/utils/cmd_path_utils.c
+++ srcs/exec/utils/cmd_path_utils.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_path_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 16:18:20 by imeulema          #+#    #+#             */
+/*   Updated: 2025/10/28 16:18:39 by imeulema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../../incl/minishell.h"
 
 int	is_lone_redir_node(t_ast *node);
 
 void	cmd_not_found(t_ast *node, char *name)
 {
-	if (!is_lone_redir(node))
+	if (!is_lone_redir_node(node))
 	{
 		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd(name, 2);
