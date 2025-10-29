8b9308f7161df3d847e26a40a1cb446603b05bc4
Ivo Meulemans <imeulema@c2r4s2.42lausanne.ch>
2025-10-29 17:19:52 +0100

    Removed prints

--- srcs/exec/expander/expander.c
+++ srcs/exec/expander/expander.c
@@ -153,8 +153,8 @@ void	expander(t_ast *node, t_cmd *cmd)
 	cmd->args[0] = (char *) ft_calloc(1, sizeof(char));
 	if (!cmd->args[0])
 		malloc_error(node, node->shell, NULL);
-	printf("Args after expander:\n");
-	int j = -1;
-	while (cmd->args[++j])
-		printf("arg[%d]: '%s'\n", j, cmd->args[j]);
+//	printf("Args after expander:\n");
+//	int j = -1;
+//	while (cmd->args[++j])
+//		printf("arg[%d]: '%s'\n", j, cmd->args[j]);
 }
